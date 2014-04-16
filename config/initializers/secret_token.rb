f1e14fb73500e2345e7cc3c19b62d27b47da4f98
Wang Xin <wilxnwang@gmail.com>
2014-04-16 15:27:28 +0800

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeavesCom::Application.config.secret_key_base = '1f3dcbe15bbbc26d33cc4c71d063c02f2d8e79ebce03a629dfce7d788c9c61701ca012d65a9c6008049a5b136caf23520c3986442a6250429f92f336a536987e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LeavesCom::Application.config.session_store :cookie_store, key: '_Leaves_com_session'
