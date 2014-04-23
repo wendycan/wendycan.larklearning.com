a9115164544c9b93cc28fd15e7458bdedc2368a3
Wendy Wang <wilxnwang@gmail.com>
2014-04-23 11:30:00 +0800

    Add demo chart to temp

--- app/assets/javascripts/temp.js
+++
@@ -1,2 +0,0 @@
-//= require temp_controller
-//= require temp_app
--- vendor/assets/javascripts/vendor.js
+++ vendor/assets/javascripts/vendor.js
@@ -1,2 +1,4 @@
 //= require angular/angular
 //= require angular-route/angular-route
+//= require d3/d3
+//= require rickshaw/rickshaw
