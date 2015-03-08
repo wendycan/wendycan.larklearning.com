cba5e492299028806cf438e6b590c67ec821f863
Wendy Wang <wilxnwang@gmail.com>
2015-03-08 11:19:55 +0800

    Add bill new

--- app/assets/javascripts/bills-backbone/views/new-bill-view.js
+++ app/assets/javascripts/bills-backbone/views/new-bill-view.js
@@ -12,11 +12,24 @@ var app = app || {};
 
     template: _.template($('#t-new-bill').html()),
 
-
     // Re-render the titles of the todo item.
     render: function () {
       $('#billsapp').html(_.template($('#t-new-bill').html()));
+      $('#new-bill-btn').click(this.createBill);
       return this;
+    },
+
+    createBill: function () {
+      var form = $('#new-bill');
+      var data = {
+        title: form.find('#title').val(),
+        people: form.find('#people').val(),
+        way: form.find('#way').val(),
+        group: form.find('#group').val(),
+        bank: form.find('#bank').val(),
+        money: form.find('#money').val()
+      };
+      console.log(data);
     }
 
   });
